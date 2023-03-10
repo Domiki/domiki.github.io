57d01db895799ed9a2c0a7c67dd5d59e46db0b3b
domiki <ando@Samsung-Galaxybook-Ion.local>
2023-03-10 20:51:39 +0900

    Modify blog to my liking & make first page

--- README.md
+++
@@ -1,1 +0,0 @@
-# domiki.github.io
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "알고리즘",
+        "excerpt":"좋은 알고리즘이란? 알고리즘이란? 컴퓨터 알고리즘: 컴퓨터가 어떤 문제를 해결하기 위해서 컴퓨터가 이해할 수 있는 방식으로 정리되어 있는 해결 방법 하나의 문제, 여러 가지 알고리즘 탐색: 저장된 정보들 중에서 원하는 값을 찾는 것 선형 탐색 알고리즘(linear search algorithm) 정렬이 된 리스트 def linear_search(element, some_list): for i in range(len(some_list)): if some_list[i] ==...","categories": ["Algorithm"],
+        "tags": [],
+        "url": "/algorithm/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98/",
+        "teaser": null
+      }]
