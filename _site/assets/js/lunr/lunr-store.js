5dbb970cfc73ca59cc6e677081890e963afef34e
domiki <ando@Samsung-Galaxybook-Ion.local>
2023-03-16 17:13:21 +0900

    Make new post about data structure

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "알고리즘",
-        "excerpt":"좋은 알고리즘이란? 알고리즘이란? 컴퓨터 알고리즘: 컴퓨터가 어떤 문제를 해결하기 위해서 컴퓨터가 이해할 수 있는 방식으로 정리되어 있는 해결 방법 하나의 문제, 여러 가지 알고리즘 탐색: 저장된 정보들 중에서 원하는 값을 찾는 것 선형 탐색 알고리즘(linear search algorithm) 정렬이 된 리스트 def linear_search(element, some_list): for i in range(len(some_list)): if some_list[i] ==...","categories": ["Algorithm"],
+        "excerpt":"좋은 알고리즘이란? 알고리즘이란? 컴퓨터 알고리즘: 컴퓨터가 어떤 문제를 해결하기 위해서 컴퓨터가 이해할 수 있는 방식으로 정리되어 있는 해결 방법 하나의 문제, 여러 가지 알고리즘 탐색: 저장된 정보들 중에서 원하는 값을 찾는 것 선형 탐색 알고리즘(linear search algorithm) 정렬이 된 리스트 def linear_search(element, some_list): for i in range(len(some_list)): if some_list[i] ==...","categories": ["Coding","Computer Science","Algorithm"],
         "tags": [],
-        "url": "/algorithm/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98/",
+        "url": "/coding/computer%20science/algorithm/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98/",
+        "teaser": null
+      },{
+        "title": "자료구조",
+        "excerpt":"기본 자료 구조들 자료 구조란? 자료 구조: 데이터의 효율적인 접근 및 조작을 가능하게 하는 저장 및 관리 방식 컴퓨터가 데이터를 저장하는 방법 데이터가 저장하는 곳 스토리지(Storage): 데이터가 영구적으로 저장되는 곳 데이터를 저장하는 데 오래 걸린다. 데이터를 받아오는 데 오래 걸린다. 메모리(Memory): 데이터가 임시로 저장되는 곳 ⇒ 메모리 데이터를 저장하는 데...","categories": ["Coding","Computer Science","Data Structure"],
+        "tags": [],
+        "url": "/coding/computer%20science/data%20structure/%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0/",
         "teaser": null
       }]
